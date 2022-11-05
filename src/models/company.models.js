b28201908535f2488829cfdd479ac1c6b4643e8f
Estefanny Leyva <fanniluley88@gmail.com>
2022-11-05 00:42:49 -0600

    add adrees model

--- src/models/company.models.js
+++ src/models/company.models.js
@@ -1,37 +1,75 @@
 import mongoose from "mongoose";
 
 // Schema de company
+const AddressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    required: true,
+  },
+  numExt: {
+    type: String,
+    required: true,
+  },
+  numInt: {
+    type: String,
+    required: true,
+  },
+  municipality: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  cp: {
+    type: String,
+    required: true,
+  },
+  latitud: {
+    type: Number,
+    required: true,
+  },
+  longitud: {
+    type: Number,
+    required: true,
+  },
+});
 
 const companySchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     match: /.*@.*\..*/,
-    trim: true
+    trim: true,
   },
   password: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   plan: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     enum: ["basic", "premium"],
-    default: "basic"
+    default: "basic",
   },
   verificationLevel: {
     type: Number,
-    required: true
+    required: true,
   },
   brandName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   rfc: {
     type: String,
-    trim: true
+    trim: true,
   },
   type: {
     type: String,
@@ -44,53 +82,52 @@ const companySchema = new mongoose.Schema({
       "Discoteca/Antro",
       "Ocio/Entetenimieto",
     ],
-    required: true
+    required: true,
   },
   rfcName: {
     type: String,
-    trim: true
+    trim: true,
   },
   legalRepresentative: {
-    type: String
-    trim: true
+    type: String,
+    trim: true,
   },
   description: {
     type: String,
     trim: true,
-    required: true
+    required: true,
   },
   image: [
     {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
   ],
   discounts: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "discounts"
+      ref: "discounts",
     },
   ],
   events: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "events"
+      ref: "events",
     },
   ],
   ranking: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "ranking"
+      ref: "ranking",
     },
   ],
   address: {
-    type: Object,
+    address: AddressSchema,
     required: true,
-    trim: true
   },
   avatar: {
-    type: String
+    type: String,
   },
 });
 
