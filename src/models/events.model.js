9e9b8c62379dd904eeaf512507f0ef6a964c827d
Victor <victorcosmema@gmail.com>
2022-11-04 19:44:49 -0600

    Add events model

---
+++ src/models/events.model.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+
+// Event's schema
+
+const eventSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        required: true
+        // options
+    },
+    timeStart: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    timeEnd: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    haveCost: {
+        type: Boolean,
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    capacity: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        minLenght: 90,
+        maxLenght: 300
+    },
+    images: {
+        type: String,
+        trim: true
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "companies",
+        required: true
+    },
+    attendance: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    }
+});
+
+// Create model
+// 'events' (reference collection name) 'eventSchema (schema)
+const Event = mongoose.model('events', eventSchema);
+
+export { Event };
