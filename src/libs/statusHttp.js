9f2703124048142e670c90b861a0cc773d7ab634
Estefanny Leyva <fanniluley88@gmail.com>
2022-11-08 18:06:56 -0600

    add base

--- src/libs/statusHttp.js
+++ src/libs/statusHttp.js
@@ -1,8 +1,7 @@
-// class StatusHttp extends Error {
-//     constructor (message, status) {
-//       super(message),
-//       this.status = status || 500
-//     }
-//   };
-  
-//   export { StatusHttp };
+class StatusHttp extends Error {
+  constructor(message, status) {
+    super(message), (this.status = status || 500);
+  }
+}
+
+export { StatusHttp };
