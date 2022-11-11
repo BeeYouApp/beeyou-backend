9679502efd5d2437b844d83889eb63a675aa1926
annie <any_prettier_3@hotmail.com>
2022-11-10 23:30:07 -0600

    add bcrypt package

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,25 +1,25 @@
-// import jwt from '../libs/jwt.js';
+import jwt from '../libs/jwt.js';
 
-// function auth (request, response, next) {
-//   try {
-//     const { authorization: token } = request.headers
+function auth (request, response, next) {
+  try {
+    const { authorization: token } = request.headers
 
-//     const tokenDecoded = jwt.verify(token)
-//     console.log(tokenDecoded) // {id: ''}
+    const tokenDecoded = jwt.verify(token)
+    console.log(tokenDecoded) // {id: ''}
 
-//     if (!tokenDecoded) throw new Error('No autorizado D:')
-//     request.userCurrent = tokenDecoded.id
-//     request.roleCurrent = tokenDecoded.role
-//     next()
-//   } catch (error) {
-//     response.status(401)
-//     response.json({
-//       success: false,
-//       message: 'No autorizado u.u',
-//       error: error.message
-//     })
-//   }
-// };
+    if (!tokenDecoded) throw new Error('No autorizado D:')
+    request.userCurrent = tokenDecoded.id
+    request.roleCurrent = tokenDecoded.role
+    next()
+  } catch (error) {
+    response.status(401)
+    response.json({
+      success: false,
+      message: 'No autorizado u.u',
+      error: error.message
+    })
+  }
+};
 
-// export { auth };
+export { auth };
 /* Validación si existe una credencial (login) */
