5e4f0db424e63970c50a3c98c919f977e2d63beb
Victor <victorcosmema@gmail.com>
2022-11-13 13:29:24 -0600

    Fix auth router process

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,7 +3,7 @@ import * as authUseCases from '../useCases/auth.use.js';
 
 const router = express.Router();
 
-router.post('/login', async (request, response, next) => {
+router.user('/login', async (request, response, next) => {
     try {
         const {email, password} = request.body;
         const token = await authUseCases.login(email, password);
